cfa360abdbc035356fdff6a55c154b63a21d9cff
IvanBlz <42589503+IvanBlz@users.noreply.github.com>
2020-08-14 17:09:11 -0500

    Update conexion.js

--- src/conexion.js
+++ src/conexion.js
@@ -3,9 +3,9 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host: 'localhost',
-        user: 'query',
-        password: 'I7340v0856*@',
-        database: '01_07_2020_development_applications'
+        user: 'root',
+        password: '',
+        database: 'Desarrollo'
     }
 
 };
